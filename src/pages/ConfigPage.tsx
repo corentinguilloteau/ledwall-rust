4b40bd209911cb79a404c9893afec05ffbabb79c
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2022-03-28 11:40:01 +0200

    Fixed config page flexbox

--- src/pages/ConfigPage.tsx
+++ src/pages/ConfigPage.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from "@mantine/core";
+import { Button, Grid, Group, Tabs } from "@mantine/core";
 import React from "react";
 import { Plus } from "tabler-icons-react";
 import Page from "./Page";
@@ -7,12 +7,22 @@ class ConfigPage extends React.Component {
 	render() {
 		return (
 			<Page title="Configuration">
-				<Tabs>
-					<Tabs.Tab label="Slice 1"></Tabs.Tab>
-					<Tabs.Tab label="Slice 2"></Tabs.Tab>
-					<Tabs.Tab label="Slice 3"></Tabs.Tab>
-					<Tabs.Tab icon={<Plus size="1rem"></Plus>}></Tabs.Tab>
-				</Tabs>
+				<Grid style={{ flex: "1", flexDirection: "column" }}>
+					<Grid.Col style={{ flex: "1 0 auto" }}>
+						<Tabs>
+							<Tabs.Tab label="Slice 1"></Tabs.Tab>
+							<Tabs.Tab label="Slice 2"></Tabs.Tab>
+							<Tabs.Tab label="Slice 3"></Tabs.Tab>
+							<Tabs.Tab icon={<Plus size="1rem"></Plus>}></Tabs.Tab>
+						</Tabs>
+					</Grid.Col>
+					<Grid.Col style={{ flex: "0 1 0" }}>
+						<Group position="right">
+							<Button>Enregistrer</Button>
+							<Button>Charger</Button>
+						</Group>
+					</Grid.Col>
+				</Grid>
 			</Page>
 		);
 	}
