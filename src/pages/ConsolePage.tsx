33765ad6f3fc10791c63aec142b559779efd7793
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2022-03-28 11:33:24 +0200

    Refactored page container

--- src/pages/ConsolePage.tsx
+++ src/pages/ConsolePage.tsx
@@ -1,20 +1,12 @@
 import { Group, Paper, Title, Text, ScrollArea, Grid, Space } from "@mantine/core";
 import { maxHeight } from "@mui/system";
 import React from "react";
+import Page from "./Page";
 
 class ConsolePage extends React.Component {
 	render() {
 		return (
-			<Grid
-				sx={(theme) => ({
-					flexDirection: "column",
-					flexWrap: "nowrap",
-					flex: 1,
-					color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
-				})}
-				m="0">
-				<Title order={1}>Console</Title>
-				<Space h="md"></Space>
+			<Page title="Console">
 				<Paper shadow="xs" p="md" component={ScrollArea} type="auto">
 					<Text>Test</Text>
 					<Text>Test</Text>
@@ -75,7 +67,7 @@ class ConsolePage extends React.Component {
 					<Text>Test</Text>
 					<Text>Test</Text> */}
 				</Paper>
-			</Grid>
+			</Page>
 		);
 	}
 }
