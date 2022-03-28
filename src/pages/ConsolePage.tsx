ca4a8df6f926cc1916002e7db72f274c6b09e22b
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2022-03-28 15:22:31 +0200

    Added borders to the console

--- src/pages/ConsolePage.tsx
+++ src/pages/ConsolePage.tsx
@@ -7,7 +7,18 @@ class ConsolePage extends React.Component {
 	render() {
 		return (
 			<Page title="Console">
-				<Paper shadow="xs" p="md" component={ScrollArea} type="auto" style={{ flex: "1" }}>
+				<Paper
+					shadow="xs"
+					p="md"
+					component={ScrollArea}
+					type="auto"
+					style={{ flex: "1" }}
+					sx={(theme) => ({
+						border: `1px solid ${
+							theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
+						}`,
+						borderRadius: `${theme.radius["md"]}px`,
+					})}>
 					<Text>Test</Text>
 					<Text>Test</Text>
 					<Text>Test</Text>
