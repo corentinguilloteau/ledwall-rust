8808893314065cb0c778008b5e5e9ccb14ab1742
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2022-03-31 01:25:03 +0200

    Added guide page

--- src/pages/GuidePage.tsx
+++ src/pages/GuidePage.tsx
@@ -1,8 +1,27 @@
+import { Paper, ScrollArea, Text } from "@mantine/core";
 import React from "react";
+import Page from "./Page";
 
 class GuidePage extends React.Component {
 	render() {
-		return <h1>Guide</h1>;
+		return (
+			<Page title="Guide">
+				<Paper
+					shadow="xs"
+					p="md"
+					component={ScrollArea}
+					type="auto"
+					style={{ flex: "1" }}
+					sx={(theme) => ({
+						border: `1px solid ${
+							theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
+						}`,
+						borderRadius: `${theme.radius["md"]}px`,
+					})}>
+					<Text>Guide </Text>
+				</Paper>
+			</Page>
+		);
 	}
 }
 
